cd867fbafea861f496074d9dfbdb973bdd487c80
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-10-09 14:52:15 +0200

    yepp

--- main/firstproject.c
+++ main/firstproject.c
@@ -5,40 +5,70 @@
 #include "esp_log.h"
 #include "esp_random.h"
 
-// https://wokwi.com/projects/378039853550046209
+// https://wokwi.com/projects/378092099167418369
 
 
 
-#define LED_PIN 26
-#define BUTTON_PIN 25
+#define SEND_BUTTON_PIN 25
+#define RECEIVE_BUTTON_PIN 26
 
-// void led1funktionen(){
-//     while(1){
-//         gpio_set_level(LED1_PIN,1 );
-//         vTaskDelay(1000 / portTICK_PERIOD_MS);
-//         gpio_set_level(LED1_PIN,0 );
-//         vTaskDelay(1000 / portTICK_PERIOD_MS);
-//     }
-// }
+int getTemperature(){
+    ESP_LOGI("SEND", "Getting temp from sensor");
+    return esp_random() % 30;
+}
+
+void receiveTask(){
+    gpio_reset_pin(RECEIVE_BUTTON_PIN);
+    gpio_set_pull_mode(RECEIVE_BUTTON_PIN, GPIO_PULLUP_ONLY);
+    int currentValue = gpio_get_level(RECEIVE_BUTTON_PIN);
+    int lastValue = 0;
+
+     while(1){
+        lastValue = currentValue;
+        //Check button
+        currentValue = gpio_get_level(RECEIVE_BUTTON_PIN);
+        //ESP_LOGE("main","Press %d", currentValue);
+        if(lastValue == 1 && currentValue == 0){
+            vTaskDelay(20 / portTICK_PERIOD_MS);
+            currentValue = gpio_get_level(RECEIVE_BUTTON_PIN);
+            if(currentValue == 0){
+                ESP_LOGE("RECEIVE","Press");
+            }
+        }
+     }
+}
 
 
-// void led2funktionen(){
-//     while(1){
-//         gpio_set_level(LED2_PIN,1 );
-//         vTaskDelay(700 / portTICK_PERIOD_MS);
-//         gpio_set_level(LED2_PIN,0 );
-//         vTaskDelay(200 / portTICK_PERIOD_MS);
-//     }
-// }
+
+void sendTask(){
+    gpio_reset_pin(SEND_BUTTON_PIN);
+    gpio_set_pull_mode(SEND_BUTTON_PIN, GPIO_PULLUP_ONLY);
+    int currentValue = gpio_get_level(SEND_BUTTON_PIN);
+    int lastValue = 0;
+
+     while(1){
+        lastValue = currentValue;
+        //Check button
+        currentValue = gpio_get_level(SEND_BUTTON_PIN);
+        //ESP_LOGE("main","Press %d", currentValue);
+        if(lastValue == 1 && currentValue == 0){
+            vTaskDelay(20 / portTICK_PERIOD_MS);
+            currentValue = gpio_get_level(SEND_BUTTON_PIN);
+            if(currentValue == 0){
+                //Get value
+                int temp = getTemperature();
+                ESP_LOGE("SEND","Press %d",  temp);
+            }
+        }
+     }
+}
+
+
 
 
 
 void app_main(void)
 {
-    gpio_reset_pin(LED_PIN);
-    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
-    gpio_reset_pin(BUTTON_PIN);
-    gpio_set_pull_mode(BUTTON_PIN, GPIO_PULLUP_ONLY);
     //gpio_set_direction(BUTTON_PIN, GPIO_MODE_INPUT_OUTPUT);
     //gpio_set_level(BUTTON_PIN,1 );
 
@@ -46,51 +76,34 @@ void app_main(void)
     //     led1funktionen,     // Function to implement the task
     //     "led1task",   // Name of the task
     //     1000,      // Stack size in words
-    //     NULL,      // Task input parameter
+    //     NULL,      // Task input parameter 
     //     0,         // Priority of the task
     //     NULL,      // Task handle.
     //     1          // Core where the task should run
     // );        
-    // xTaskCreatePinnedToCore (
-    //     led2funktionen,     // Function to implement the task
-    //     "led2task",   // Name of the task
-    //     1000,      // Stack size in words
-    //     NULL,      // Task input parameter
-    //     0,         // Priority of the task
-    //     NULL,      // Task handle.
-    //     1          // Core where the task should run
-    // );        
-    // xTaskCreatePinnedToCore (
-    //     led3funktionen,     // Function to implement the task
-    //     "led3task",   // Name of the task
-    //     1000,      // Stack size in words
-    //     NULL,      // Task input parameter
-    //     0,         // Priority of the task
-    //     NULL,      // Task handle.
-    //     1          // Core where the task should run
-    // );        
+    xTaskCreatePinnedToCore (
+        receiveTask,     // Function to implement the task
+        "receiveTask",   // Name of the task
+        4096,      // Stack size in words
+        NULL,      // Task input parameter
+        0,         // Priority of the task
+        NULL,      // Task handle.
+        1          // Core where the task should run
+    );        
+    xTaskCreatePinnedToCore (
+        sendTask,     // Function to implement the task
+        "sendTask",   // Name of the task
+        4096,      // Stack size in words
+        NULL,      // Task input parameter
+        0,         // Priority of the task
+        NULL,      // Task handle.
+        1          // Core where the task should run
+    );        
 
-    int currentValue = gpio_get_level(BUTTON_PIN);
-    int lastValue = 0;
-    int ledState = 0;
     while(1){
-        lastValue = currentValue;
-        //Check button
-        currentValue = gpio_get_level(BUTTON_PIN);
-        //ESP_LOGE("main","Press %d", currentValue);
-        if(lastValue == 1 && currentValue == 0){
-            vTaskDelay(20 / portTICK_PERIOD_MS);
-            currentValue = gpio_get_level(BUTTON_PIN);
-            if(currentValue == 0){
-                int times = esp_random()%10 + 1;
-                int between = esp_random()%100;
-                ESP_LOGE("main","Press %d %d",times,between);
-                ledState = !ledState;
-                gpio_set_level(LED_PIN,ledState);
-            }
-        }
-        //vTaskDelay(400 / portTICK_PERIOD_MS);
-
+        
+        vTaskDelay(400 / portTICK_PERIOD_MS);
     }
+
 
 }
