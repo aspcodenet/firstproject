ec2611ca3870a658a1ba37c338252d6115e1cbc8
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-10-12 11:12:34 +0200

    a

--- main/firstproject.c
+++ main/firstproject.c
@@ -28,35 +28,58 @@ static const char *TAG = "mqttwss_example";
 #define TOPIC "stefanholmberg/feeds/testfeed"
 
 
+int getTemperature(){
+    ESP_LOGI("SEND", "Getting temp from sensor");
+    return esp_random() % 30;
+}
+#define SEND_BUTTON_PIN 25
+#define RECEIVE_BUTTON_PIN 26
+
+esp_mqtt_client_handle_t client;
+
+void sendTask(){
+    gpio_reset_pin(SEND_BUTTON_PIN);
+    gpio_set_pull_mode(SEND_BUTTON_PIN, GPIO_PULLUP_ONLY);
+    int currentValue = gpio_get_level(SEND_BUTTON_PIN);
+    int lastValue = 0;
+
+     while(1){
+        lastValue = currentValue;
+        //Check button
+        currentValue = gpio_get_level(SEND_BUTTON_PIN);
+        //ESP_LOGE("main","Press %d", currentValue);
+        if(lastValue == 1 && currentValue == 0){
+            vTaskDelay(20 / portTICK_PERIOD_MS);
+            currentValue = gpio_get_level(SEND_BUTTON_PIN);
+            if(currentValue == 0){
+                //Get value
+                int temp = getTemperature();
+                ESP_LOGE("SEND","Press %d",  temp);
+                char text[20];
+                sprintf(text,"%d",temp);
+                int id = esp_mqtt_client_publish(client, TOPIC, text  , 0, 0, 0);
+                ESP_LOGE("SEND","ID %d",  id);
+            }
+        }
+     }
+}
+
 
 static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
 {
-    esp_mqtt_client_handle_t client = event->client;
+    client = event->client;
     int msg_id;
     // your_context_t *context = event->context;
     switch (event->event_id) {
     case MQTT_EVENT_CONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        msg_id = esp_mqtt_client_subscribe(client, TOPIC, 0);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        // msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-        // ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        // msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-        // ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
         break;
     case MQTT_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
         break;
 
     case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        msg_id = esp_mqtt_client_publish(client, TOPIC, "data", 0, 0, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
         break;
     case MQTT_EVENT_UNSUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
         break;
     case MQTT_EVENT_PUBLISHED:
         ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
@@ -113,5 +136,20 @@ void app_main(void)
 
     esp_event_loop_create_default();
 
+    xTaskCreatePinnedToCore (
+        sendTask,     // Function to implement the task
+        "sendTask",   // Name of the task
+        4096,      // Stack size in words
+        NULL,      // Task input parameter
+        0,         // Priority of the task
+        NULL,      // Task handle.
+        1          // Core where the task should run
+    );        
+
+
+
     mqtt_app_start();
+    while(1){
+        vTaskDelay(10000);
+    }
 }
