b282a7cac12abfbb64c636388b07c3cc2ca1f6e8
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-10-12 10:56:13 +0200

    server

--- main/firstproject.c
+++ main/firstproject.c
@@ -1,64 +1,103 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
 #include <string.h>
-#include <stdlib.h>
-#include "esp_log.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
 #include "nvs_flash.h"
 #include "esp_event.h"
 #include "esp_netif.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
 
-#include "esp_http_client.h"
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
 #include "connect_wifi.h"
 
-static const char *TAG = "HTTP_CLIENT";
+static const char *TAG = "mqttwss_example";
+#define CONFIG_BROKER_URI "mqtt://test.mosquitto.org"
 
-char api_key[] = "RQZXY61HSJURBPZL";
+//#define CONFIG_BROKER_URI "mqtt://stefan.holmberg:testfeed@io.adafruit.com"
+#define TOPIC "stefanholmberg/feeds/testfeed"
 
-char message[] = "Hello This is a test message";
 
-void twilio_send_sms(void *pvParameters)
+
+static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
 {
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    // your_context_t *context = event->context;
+    switch (event->event_id) {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        msg_id = esp_mqtt_client_subscribe(client, TOPIC, 0);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
-    char twilio_url[200];
-    snprintf(twilio_url,
-             sizeof(twilio_url),
-             "%s%s%s",
-             "https://api.thingspeak.com/update?api_key=",
-             api_key,
-             "&field1=22&field2=66");
+        // msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+        // ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
-    esp_http_client_config_t config = {
-        .url = twilio_url,
-        .method = HTTP_METHOD_GET   };
+        // msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+        // ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
 
-    esp_http_client_handle_t client = esp_http_client_init(&config);
-    esp_http_client_set_header(client, "Content-Type", "application/x-www-form-urlencoded");
-
-
-
-    esp_err_t err = esp_http_client_perform(client);
-
-    if (err == ESP_OK)
-    {
-        int status_code = esp_http_client_get_status_code(client);
-        if (status_code == 201)
-        {
-            ESP_LOGI(TAG, "Message sent Successfully");
-        }
-        else
-        {
-            ESP_LOGI(TAG, "Message sent Failed");
-        }
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        msg_id = esp_mqtt_client_publish(client, TOPIC, "data", 0, 0, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
     }
-    else
-    {
-        ESP_LOGI(TAG, "Message sent Failed");
-    }
-    esp_http_client_cleanup(client);
-    vTaskDelete(NULL);
+    return ESP_OK;
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    /* The argument passed to esp_mqtt_client_register_event can de accessed as handler_args*/
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32, base, event_id);
+    mqtt_event_handler_cb(event_data);
+}
+
+static void mqtt_app_start(void)
+{
+    const esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = CONFIG_BROKER_URI,
+        //  .credentials.username="stefanholmberg",
+        //  .credentials.authentication.password="testfeed"
+        //.broker.verification.certificate = (const char *)mqtt_eclipseprojects_io_pem_start,
+    };
+
+    ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+
+    esp_mqtt_client_start(client);
 }
 
 void app_main(void)
@@ -71,8 +110,8 @@ void app_main(void)
 	}
 	ESP_ERROR_CHECK(ret);
 	connect_wifi();
-	if (wifi_connect_status)
-	{
-		xTaskCreate(twilio_send_sms, "twilio_send_sms", 8192, NULL, 6, NULL);
-	}
+
+    esp_event_loop_create_default();
+
+    mqtt_app_start();
 }
