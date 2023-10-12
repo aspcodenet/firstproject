99359892984b7e6ae5d876b95478e374d35cf8c3
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-10-12 12:45:18 +0200

    aaa

--- main/firstproject.c
+++ main/firstproject.c
@@ -52,8 +52,6 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
 
     case MQTT_EVENT_SUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        //msg_id = esp_mqtt_client_publish(client, TOPIC, "data-jepp",0, 0, 1);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
         break;
     case MQTT_EVENT_UNSUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
